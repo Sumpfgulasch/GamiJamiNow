c8bd79576d70a624105aa06fa40f66ae909e08c1
Kala <kala476@gmail.com>
2021-03-21 14:03:11 +0100

    updated the player prefab, repushed the scene with all prefabs inside

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public System.Action OnLosschieﬂen;
+	public System.Action OnLosschiessen; 
 	public System.Action OnTreffen;
 
 	public GameObject playerRepresentation;
